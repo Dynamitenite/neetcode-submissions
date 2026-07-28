14ca2a69d404a0820f3799dd54ff6406fd9ab1ce
Aakaash Pavangat <aakaashanoop@gmail.com>
2026-07-28 22:08:25 +0530

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        actual_sum = (n*(n+1))//2 
+        array_sum = 0
+
+        for num in nums:
+            array_sum += num
+
+        return actual_sum - array_sum
+
+        
+
+        
